ae7fa19e2b07d8691475d4603addf88050605f2e
Evan Furniss <evanfurniss@gmail.com>
2021-01-11 14:34:32 -0600

    Set files to match best practice set up

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+//Using the frame work set up for us, we need to create rows that identify the hours of your day.
+    //Set up hours starting at the beginning of your work day and a new hour each hour until your last hour of the day.
+    //Within each hour, you should be able to input text in the main display for that hour and save it to local storage.
+    //The current hour is highlighted Green (or some color), past hours should be grey (or some unique color), and upcoming hours should be red(or some unique color)
+
+// A running clock should be operating in the background.
+    //When the clock reaches a new hour, the current hour will move up
+    // when the current hour moves up, the previous hour should grey, the current should green, the future should remain red
+
+//Each hour should have a save button that will save the information of that one specific hour to local stoarge
+    //If the user creates multiple hours of information but only saves for one of those hours, the other hours will not save
+    //Maybe try to create a save all button that will save each hour of information.
